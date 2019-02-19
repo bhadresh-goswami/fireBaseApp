3b0bbee149c34bc962991d5a2127a9bf1668cde6
bhadresh-goswami <tops.bhadresh@gmail.com>
2019-02-19 18:48:12 +0530

    done

--- fireBaseApp/ViewController.swift
+++ fireBaseApp/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var txtUserName: UITextField!
+    @IBOutlet var txtUserPassword: UITextField!
+    var ref:DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        ref = Database.database().reference()
+        var refHandle = ref.observe(DataEventType.value, with: { (snapshot) in
+            let postDict = snapshot.value as? [String : AnyObject] ?? [:]
+            print(postDict)
+        })
     }
 
+    @IBAction func tapToSaveData(_ sender: UIButton) {
+        ref.child("Harsha").childByAutoId().setValue(["name":txtUserName.text!,"password":txtUserPassword.text!])
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
